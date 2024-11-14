d179c0c6e132d28957c5876a6cda905a5f730939
Sebastião Rusu <20220905@iade.pt>
2024-11-14 01:19:37 +0000

    Base de dados com Docker com sucesso

--- data/sql/database.sql
+++ data/sql/database.sql
@@ -2,14 +2,47 @@ CREATE TABLE IF NOT EXISTS userVyT(
     user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(30) ,
     email VARCHAR(50) UNIQUE ,
-    password_hash VARCHAR(100) ,
-);
+    password_hash VARCHAR(100)
+    );
 
 CREATE TABLE IF NOT EXISTS tokens(
     token_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
+    token_user_id INT NOT NULL,
     token VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES userVyT(user_id)
-);
+    );
+CREATE TABLE if not exists tarefas(
+    tarefa_id serial primary key,
+    tarefa_user_id int,
+                                      tarefa_titulo VARCHAR(50),
+    tarefa_descricao VARCHAR(50),
+    tarefa_prioridade int,
+    tarefa_importancia int,
+    tarefa_preferencia_tempo time,
+    tarefa_criacao_at timestamp,
+    FOREIGN KEY (tarefa_user_id) REFERENCES userVyT(user_id)
+
+    );
+
+CREATE TABLE if not exists feedback(
+    feedback_id serial primary key,
+    feedback_user_id int,
+    feedback_tarefa_id int,
+    feedback_performance int,
+    feedback_comentario VARCHAR(50),
+    FOREIGN KEY (feedback_user_id) REFERENCES userVyT(user_id),
+    FOREIGN KEY (feedback_tarefa_id) REFERENCES tarefas(tarefa_id)
+    );
+
+CREATE TABLE if not exists horario(
+    horario_id serial primary key,
+    horario_user_id int,
+    horario_tarefa_id int,
+    horario_data date,
+    horario_tempo time,
+    horario_status VARCHAR(50),
+    FOREIGN KEY (horario_user_id) REFERENCES userVyT(user_id),
+    FOREIGN KEY (horario_tarefa_id) REFERENCES tarefas(tarefa_id)
+    );
