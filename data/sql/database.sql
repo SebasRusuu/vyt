4b45c373329f10933e6f9676cb805cf4f767caad
tomaslebre <20220907@iade.pt>
2024-12-31 00:54:04 +0000

    Back-end de criar tarefa e ver as tarefas de um user

--- data/sql/database.sql
+++ data/sql/database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS userVyT(
+CREATE TABLE IF NOT EXISTS uservyt(
     user_id SERIAL PRIMARY KEY not null,
     user_name VARCHAR(30) not null,
     email VARCHAR(50) UNIQUE not null,
@@ -11,18 +11,18 @@ CREATE TABLE IF NOT EXISTS tokens(
     token VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP,
-    FOREIGN KEY (token_user_id) REFERENCES userVyT(user_id)
+    FOREIGN KEY (token_user_id) REFERENCES uservyt(user_id)
     );
 CREATE TABLE if not exists tarefas(
     tarefa_id serial primary key,
     tarefa_user_id int,
-    tarefa_titulo VARCHAR(50),
-    tarefa_descricao VARCHAR(50),
+    tarefa_titulo VARCHAR(70),
+    tarefa_descricao VARCHAR(255),
     tarefa_prioridade int,
     tarefa_importancia int,
     tarefa_preferencia_tempo time,
     tarefa_criacao_at timestamp,
-    FOREIGN KEY (tarefa_user_id) REFERENCES userVyT(user_id)
+    FOREIGN KEY (tarefa_user_id) REFERENCES uservyt(user_id)
 
     );
 
@@ -32,7 +32,7 @@ CREATE TABLE if not exists feedback(
     feedback_tarefa_id int,
     feedback_performance int,
     feedback_comentario VARCHAR(50),
-    FOREIGN KEY (feedback_user_id) REFERENCES userVyT(user_id),
+    FOREIGN KEY (feedback_user_id) REFERENCES uservyt(user_id),
     FOREIGN KEY (feedback_tarefa_id) REFERENCES tarefas(tarefa_id)
     );
 
@@ -43,6 +43,6 @@ CREATE TABLE if not exists horario(
     horario_data date,
     horario_tempo time,
     horario_status VARCHAR(50),
-    FOREIGN KEY (horario_user_id) REFERENCES userVyT(user_id),
+    FOREIGN KEY (horario_user_id) REFERENCES uservyt(user_id),
     FOREIGN KEY (horario_tarefa_id) REFERENCES tarefas(tarefa_id)
     );
--- server/src/main/java/com/iade/vyt/models/User.java
+++ server/src/main/java/com/iade/vyt/models/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "userVyT")
+@Table(name = "uservyt")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
